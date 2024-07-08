ee32c2cd48da9b24f33df6f53556848f539e1dcd
im-tiket <ikhsan.mahendri@tiket.com>
2024-07-09 00:16:37 +0700

    fix: [DISNEY-PLUS] - fix MSW handler

--- src/lib/mocks/msw-handlers.ts
+++ src/lib/mocks/msw-handlers.ts
@@ -20,4 +20,26 @@ export const handlers = [
       status: statusCode,
     });
   }),
+  http.get(`${process.env.NEXT_PUBLIC_BASE_API}/books/:id`, (req) => {
+    const { id } = req.params;
+    const resultBook = BookListMock.find((item: TBook) => item?.id === Number(id));
+    let statusCode = 404;
+    if (resultBook && resultBook?.id) {
+      statusCode = 200;
+    }
+    return HttpResponse.json<TBook>(resultBook, {
+      status: statusCode,
+    });
+  }),
+  http.get(`${process.env.NEXT_PUBLIC_BASE_API}/discover/tv`, (req) => {
+    const { id } = req.params;
+    const resultBook = BookListMock.find((item: TBook) => item?.id === Number(id));
+    let statusCode = 404;
+    if (resultBook && resultBook?.id) {
+      statusCode = 200;
+    }
+    return HttpResponse.json<TBook>(resultBook, {
+      status: statusCode,
+    });
+  }),
 ];
